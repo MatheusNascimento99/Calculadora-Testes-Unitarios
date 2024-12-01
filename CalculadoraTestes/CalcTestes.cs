38a408bf456665c4cfc4a123edc844262a7946e2
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-12-01 20:54:19 -0300

    Projeto completo e testes

---
+++ CalculadoraTestes/CalcTestes.cs
@@ -0,0 +1,98 @@
+using Calculadora.Models;
+
+namespace CalculadoraTestes;
+
+public class CalcTestes
+{
+    private Metodo _metodo;
+
+    public CalcTestes()
+    {
+        _metodo = new Metodo();
+    }
+
+    //criação de valores aleatórios para testes
+    private int ValoresPseudoUsuario()
+    {
+        Random random = new Random();
+        return random.Next(-200, 200);
+    }
+
+
+    [Fact] //Soma
+    public void SomarERetornarValorCorreto()
+    {
+        //Arrange
+        int valor1 = ValoresPseudoUsuario();
+        int valor2 = ValoresPseudoUsuario();
+
+        //Act
+        int resultado = _metodo.Somar(valor1, valor2);
+
+        //Assert
+        Assert.Equal(valor1 + valor2, resultado);
+
+    }
+
+    [Fact] //Subtração
+    public void SubtrairERetornarValorCorreto()
+    {
+        // Given
+        int valor1 = ValoresPseudoUsuario();
+        int valor2 = ValoresPseudoUsuario();
+        // When
+        int resultado = _metodo.Subtrair(valor1, valor2);
+
+        // Then
+        Assert.Equal(valor1 - valor2, resultado);
+    }
+
+    [Fact] // Multiplicação
+    public void MultiplicarERetornarValorCorreto()
+    {
+        // Given
+        int valor1 = ValoresPseudoUsuario();
+        int valor2 = ValoresPseudoUsuario();
+        // When
+        int resultado = _metodo.Multiplicar(valor1, valor2);
+        // Then
+        Assert.Equal(valor1 * valor2, resultado);
+    }
+
+    [Fact] // Divisão
+    public void DividirERetornarValorCorreto()
+    {
+        // Given
+        int valor1 = ValoresPseudoUsuario();
+        int valor2 = ValoresPseudoUsuario();
+        // When
+        int resultado = _metodo.Dividir(valor1, valor2);
+        // Then
+        Assert.Equal(valor1 / valor2, resultado);
+    }
+
+    [Fact] // Resto da Divisão
+    public void VerificarRestoDaDivisaoERetornarValorCorreto()
+    {
+        // Given
+        int valor1 = ValoresPseudoUsuario();
+        int valor2 = ValoresPseudoUsuario();
+        // When
+        int resultado = _metodo.RestoDaDivisao(valor1, valor2);
+        // Then
+        Assert.Equal(valor1 % valor2, resultado);
+    }
+
+    [Fact] // Potenciação
+    public void PotenciacaoERetornarValorCorreto()
+    {
+        // Given
+        int valor1 = ValoresPseudoUsuario();
+        int valor2 = ValoresPseudoUsuario();
+        // When
+        var resultado = _metodo.Potenciacao(valor1, valor2);
+        double potencia = Math.Pow(valor1, valor2);
+        // Then
+        Assert.Equal(potencia, resultado);
+    }
+}
